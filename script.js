e149fb166f0e5d722fd9cef68702072d7ac0a139
Aman <aman.prajapati7022@gmail.com>
2023-06-25 20:04:40 +0530

    Allow user to change size

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const container = document.querySelector(".container");
 let boxLine;
 let box;
+let n;
 
-for(let j = 1; j <=16; j++){
-    boxLine = document.createElement("div");
-    boxLine.classList.add("box-line");
+function makeGrid(n){
+    container.innerHTML = "";
+    for(let j = 1; j <=n; j++){
+        boxLine = document.createElement("div");
+        boxLine.classList.add("box-line");
 
-    for(let i = 1; i <=16; i++){
-        box = document.createElement("div");
-        box.classList.add("box");
-        box.addEventListener("mouseover", (e)=>{
-            console.log(e.target);
-            e.target.classList.add("colored")
-        });
-        boxLine.appendChild(box);
+        for(let i = 1; i <=n; i++){
+            box = document.createElement("div");
+            box.classList.add("box");
+            box.addEventListener("mouseover", (e)=>{
+                console.log(e.target);
+                e.target.classList.add("colored");
+            });
+            boxLine.appendChild(box);
+        }
+
+        container.appendChild(boxLine);
     }
-    container.appendChild(boxLine);
 }
 
+makeGrid(16);
+const changeGrid = document.querySelector(".change-grid");
+changeGrid.addEventListener("click", ()=>{
+    n = +prompt("Give the size of grid you want.");
+    makeGrid(n);
+
+});
 
